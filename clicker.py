4dd1e731ce4c299cd6d15a890cab0f39632dcaab
Harry <jcceong@gmail.com>
2019-03-13 18:00:32 -0700

    changed stuff

--- clicker.py
+++ clicker.py
@@ -1,22 +1,24 @@
-import win32api, win32con, os, time
+import win32api, win32con, time
+
+def click():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 try:
     import keyboard
+
+    toggle = False
+    while True:
+        if keyboard.is_pressed("f1"):
+            toggle = not toggle
+
+        if keyboard.is_pressed("-"):
+            quit()
+
+        time.sleep(.100)
+        if toggle:
+            click()
+
 except:
-    os.system("pip install keyboard")
+    print("Error, Module Keyboard is required. Run \"pip install keyboard\" and try again")
 
-def click():
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-
-toggle = False
-while True:
-    if keyboard.is_pressed("f1"):
-        toggle = not toggle
-
-    if keyboard.is_pressed("-"):
-        quit()
-
-    time.sleep(.100)
-    if toggle:
-        click()
