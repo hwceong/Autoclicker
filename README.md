6c6347a62512eebcb4731179057ef0d3c607158d
Harry <jcceong@gmail.com>
2019-03-14 19:03:53 -0700

    clicker2.py

---
+++ README.md
@@ -0,0 +1,3 @@
+clicker2.py supports both windows and linux. Before you use open up terminal and type "pip install pynput".
+
+clicker.py is just a proof of concept and I suggest you should use it.
--- clicker.py
+++ clicker.py
@@ -1,24 +1,17 @@
-import win32api, win32con, time
+import win32api, win32con, time, keyboard
 
 def click():
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
-try:
-    import keyboard
+toggle = False
+while True:
+    if keyboard.is_pressed("f1"):
+        toggle = not toggle
 
-    toggle = False
-    while True:
-        if keyboard.is_pressed("f1"):
-            toggle = not toggle
+    if keyboard.is_pressed("-"):
+        quit()
 
-        if keyboard.is_pressed("-"):
-            quit()
-
-        time.sleep(.100)
-        if toggle:
-            click()
-
-except:
-    print("Error, Module Keyboard is required. Run \"pip install keyboard\" and try again")
-
+    time.sleep(.300)
+    if toggle:
+        click()
