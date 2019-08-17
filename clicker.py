04b5f55e3f7700af394ca0edcb5b42d770d5f10e
Harry <jcceong@gmail.com>
2019-08-17 09:35:42 -0700

    revision

--- clicker.py
+++ clicker.py
@@ -1,17 +1,48 @@
-import win32api, win32con, time, keyboard
+import threading, time
+from pynput import mouse, keyboard
 
-def click():
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+class Mouse(threading.Thread):
+    def __init__(self, delaytimer):
+        threading.Thread.__init__(self)
+        self.delaytimer = delaytimer
+        self.running = False
+        self.program_running = True
+        self.key = None
 
-toggle = False
-while True:
-    if keyboard.is_pressed("f1"):
-        toggle = not toggle
+    def toggle(self, key):
+        self.running = not self.running
+        self.key = key
 
-    if keyboard.is_pressed("-"):
-        quit()
+    def exit(self):
+        self.running = False
+        self.program_running = False
 
-    time.sleep(.300)
-    if toggle:
-        click()
+    def run(self): # NOTE: In threading.Thread, it automatically runs self.run() when initialized.
+        while self.program_running:
+            while self.running:
+                time.sleep(self.delaytimer)
+                if self.key == keyboard.KeyCode(char=","):
+                    cursor.click(mouse.Button.left,2)
+                else:
+                    cursor.click(mouse.Button.right,2)
+
+def on_press(key):
+    if key == keyboard.KeyCode(char=","):
+        mouse_thread.toggle(key)
+
+    if key == keyboard.KeyCode(char="."):
+        mouse_thread.toggle(key)
+
+    if key == keyboard.Key.esc:
+        mouse_thread.exit()
+        listener.stop()
+
+if __name__ == '__main__':
+    cursor = mouse.Controller()
+    mouse_thread = Mouse(0.1)
+    mouse_thread.start()
+
+    print("Press \",\" to toggle auto clicking. Press ESC to close to program.")
+
+    with keyboard.Listener(on_press=on_press) as listener: # NOTE: from pynput module, it the keyboard.Listener listens from keyboard input
+        listener.join()
